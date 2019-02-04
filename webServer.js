33e5ff6fd6e0d4f8009d5cbec9601bb1d8321401
saurmish <saurabhr.mishra@gmail.com>
2019-02-04 10:39:36 +0530

    Setup heroku port

--- webServer.js
+++ webServer.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -58,6 +59,6 @@ app.get('/bad',(req,res) => {
   });
 });
 
-app.listen(3000, () => {
-console.log('APP has started on port 3000');
+app.listen(port, () => {
+console.log(`APP has started on port: ${port}`);
 });
